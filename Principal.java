004a9bd7f7ae17aeb987f44cf96bc9e715036425
lola <lolita@gmail.com>
2023-11-21 15:52:47 -0500

    "Parcial #2"

--- Principal.java
+++ Principal.java
@@ -7,6 +7,7 @@ public class Principal {
         Telefono telefonoUno = new Telefono("Iphone 11 Pro max", "Telefono gama alta", 3399999, 100, "iPhone", 256, true, "Blanco", 6.5);
         Telefono telefonoDos = new Telefono("Iphone 12 Pro max", "Telefono gama muy alta", 4399999, 50, "iPhone", 256, false, "Azul", 6.7);
         Telefono telefonoTres = new Telefono("Iphone 13 Pro max", "Telefono gama demasiado alta", 5800000, 80, "iPhone", 256, false, "Rojo", 6.1);
-        Telefono telefonoCuatro = new Telefono();
+        Telefono telefonoCuatro = new Telefono("Samsung Galaxy 10 plus", "Telefono gama media", 1500000, 15, "Samsung", 512, true, "Negro", 6.5);
+        Laptop laptop = new Laptop("");
     }
 }
