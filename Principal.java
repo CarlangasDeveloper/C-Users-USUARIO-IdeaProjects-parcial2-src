1ddd63e859b59dd60324bee52f2096e62e4b8a2d
lola <lolita@gmail.com>
2023-11-21 15:38:29 -0500

    "Parcial #2"

--- Principal.java
+++ Principal.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Telefono telefono = new Telefono("Iphone 11 Pro max", "Telefono gama alta");
+    }
 }
